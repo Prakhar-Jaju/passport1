01b92e6bf298df69a5f8ef4ccf8281bff46b66e3
Prakhar Jaju <91278321+Prakhar-Jaju@users.noreply.github.com>
2023-08-25 17:23:25 +0530

    Add files via upload

---
+++ framerusercontent.com/sites/3kmxXpDuAJFwsSUjwjm7Dt/chunk-RONKVW2J.mjs
@@ -0,0 +1,4 @@
+var e=a=>({bodyClassName:"framer-body-augiA20Il",breakpoints:[{hash:"72rtr7",mediaQuery:"(min-width: 1440px)"},{hash:"1fvbfmi",mediaQuery:"(min-width: 1200px) and (max-width: 1439px)"},{hash:"zsvj0s",mediaQuery:"(min-width: 810px) and (max-width: 1199px)"},{hash:"pi82ru",mediaQuery:"(max-width: 809px)"}],description:`Your pass to 
+easy ticket 
+resell!`,elements:{BXdlm0NWN:"hero",nmKefVTkC:"get-in-touch",pWTyIXY5a:"schedule",uuAUeCsje:"schedule-row1",x_pfNnpg0:"schedule-row2"},framerSearch:{index:!0},title:"Passport",viewport:"width=device-width"}),s=e,d=1,n={exports:{default:{type:"variable",annotations:{framerContractVersion:"1"}},metadataVersion:{type:"variable",annotations:{framerContractVersion:"1"}},__FramerMetadata__:{type:"variable"}}};export{s as a,d as b,n as c};
+//# sourceMappingURL=chunk-RONKVW2J.mjs.map
